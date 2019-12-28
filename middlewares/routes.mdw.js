31b072b0b493619886f3c73a8af1b36674a2c85b
VO Quoc An <hvcgtraining@gmail.com>
2019-12-28 16:01:50 +0700

    update cat

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,8 +1,12 @@
 const categoryModel = require('../models/category.model');
 <<<<<<< HEAD
+<<<<<<< HEAD
 const productModel = require('../models/product.model');
 =======
 >>>>>>> eb01852... Category almost done hahaha
+=======
+const productModel = require('../models/product.model');
+>>>>>>> a563e14... update cat
 
 module.exports = function(app) {
     app.get('/', async function(req, res) {
@@ -10,20 +14,28 @@ module.exports = function(app) {
         var categories = await categoryModel.allWithSubCat();
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         //var product = await productModel.
             // console.log(JSON.stringify(categories));
 =======
         // console.log(JSON.stringify(categories));
 >>>>>>> 4f8dcc6... category done/menu done mfk
+=======
+        var product = await productModel.
+            // console.log(JSON.stringify(categories));
+>>>>>>> a563e14... update cat
         res.render('home', {
             lcCategories: categories
         });
         // res.render('../viewProduct/topFiveTemplate');
+<<<<<<< HEAD
 =======
         res.render('home', {
             lcCategories: categories
         });
 >>>>>>> eb01852... Category almost done hahaha
+=======
+>>>>>>> a563e14... update cat
     })
 
     app.get('/about', function(req, res) {
--- routes/category.route.js
+++ routes/category.route.js
@@ -6,10 +6,14 @@ const router = express.Router();
 
 router.get('/', async function(req, res) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
     console.log('categories');
 >>>>>>> eb01852... Category almost done hahaha
+=======
+
+>>>>>>> a563e14... update cat
     var categories = await categoryModel.allWithSubCat();
 
     res.render('../views/viewProduct/topFiveTemplate', {
