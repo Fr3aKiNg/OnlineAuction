43e24c542a919383ed614df11f8e94d09c4800fc
VO Quoc An <hvcgtraining@gmail.com>
2020-01-01 16:11:28 +0700

    method login.hbs fixed

--- routes/_account.route.js
+++ routes/_account.route.js
@@ -6,72 +6,73 @@ const config = require('../config/default.json');
 
 const router = express.Router();
 
-router.get('/login', async function (req, res) {
-  res.render('viewAccount/login', {
-    layout: false
-  });
+router.get('/login', async function(req, res) {
+    res.render('viewAccount/login', {
+        layout: false
+    });
 })
 
-router.post('/login', async function (req, res) {
-  const user = await userModel.singleByUserName(req.body.username);
-  if (user === null)
-    return res.render('viewAccount/login', {
-      layout: false,
-      err_message: 'Invalid username or password.'
-    });
+router.post('/login', async function(req, res) {
+    console.log("login");
+    const user = await userModel.singleByUserName(req.body.username);
+    if (user === null)
+        return res.render('viewAccount/login', {
+            layout: false,
+            err_message: 'Invalid username or password.'
+        });
 
-  const rs = bcrypt.compareSync(req.body.password, user.password_hash);
-  if (rs === false)
-    return res.render('viewAccount/login', {
-      layout: false,
-      err_message: 'Invalid username or password.'
-    });
+    const rs = bcrypt.compareSync(req.body.password, user.password_hash);
+    if (rs === false)
+        return res.render('viewAccount/login', {
+            layout: false,
+            err_message: 'Invalid username or password.'
+        });
 
-  delete user.password_hash;
-  req.session.isAuthenticated = true;
-  req.session.authUser = user;
+    delete user.password_hash;
+    req.session.isAuthenticated = true;
+    req.session.authUser = user;
 
-  const url = req.query.retUrl || '/';
-  res.redirect(url);
+    const url = req.query.retUrl || '/';
+    res.redirect(url);
 })
 
-router.post('/logout', async function (req, res) {
-  req.session.isAuthenticated = false;
-  req.session.authUser = null;
-  res.redirect(req.headers.referer);
+router.post('/logout', async function(req, res) {
+    req.session.isAuthenticated = false;
+    req.session.authUser = null;
+    res.redirect(req.headers.referer);
 })
 
 
 const restrict = require('../middlewares/auth.mdw');
-router.get('/profile', restrict, async function (req, res) {
-  res.render('viewAccount/profile');
+router.get('/profile', restrict, async function(req, res) {
+    res.render('viewAccount/profile');
 })
 
-router.get('/register', async function (req, res) {
-  res.render('viewAccount/register');
+router.get('/register', async function(req, res) {
+    res.render('viewAccount/register');
 })
 
-router.post('/register', async function (req, res) {
-  const hash = bcrypt.hashSync(req.body.password, config.authentication.saltRounds);
-  const dob = moment(req.body.dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
-  const entity = {
-    username: req.body.username,
-    password_hash: hash,
-    name: req.body.name,
-    email: req.body.email,
-    dob,
-    permission: 0
-  }
-  const ret = await userModel.add(entity);
-  res.render('viewAccount/register');
+router.post('/register', async function(req, res) {
+    const hash = bcrypt.hashSync(req.body.password, config.authentication.saltRounds);
+    const dob = moment(req.body.dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    const entity = {
+        username: req.body.username,
+        password_hash: hash,
+        name: req.body.name,
+        email: req.body.email,
+        dob,
+        permission: 0
+    }
+    const ret = await userModel.add(entity);
+    res.render('viewAccount/register');
 })
 
-router.get('/is-available', async function (req, res) {
-  const user = await userModel.singleByUserName(req.query.user);
-  if (!user)
-    return res.json(true);
+router.get('/is-available', async function(req, res) {
+    const user = await userModel.singleByUserName(req.query.user);
+    if (!user)
+        return res.json(true);
 
-  res.json(false);
+    res.json(false);
 })
 
 module.exports = router;
