1f796d8400aeea8317a6bbfd550eed338fe3d6c4
VO Quoc An <hvcgtraining@gmail.com>
2020-01-09 00:09:26 +0700

    bid history

--- routes/product.route.js
+++ routes/product.route.js
@@ -7,8 +7,10 @@ const router = express.Router();
 
 router.get('/:id', async function(req, res) {
     const row = await productModel.single(req.params.id);
+    const offers = await productModel.offers(req.params.id);
     res.render('viewProduct/product', {
-        product: row
+        product: row,
+        offer: offers
     })
 })
 
