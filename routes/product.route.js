a37de01db7d342ecbbc117d61a2cc753fed4e647
chihuynhminh <chihuynhminh@gmail.com>
2020-01-08 23:58:43 +0700

    format time and price

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const productModel = require('../models/product.model');
 const config = require('../config/default.json');
+const moment = require('moment')
+const numeral = require('numeral')
 
 const router = express.Router();
 
 
 router.get('/:id', async function(req, res) {
     const row = await productModel.single(req.params.id);
+    row.posted_time = moment(row.posted_time).format('LLL');
+    row.end_time = moment(row.end_time).fromNow();
+    row.buy_now_price_string = numeral(row.buy_now_price).format('0,0');
+    row.price_step_string = numeral(row.price_step).format('0,0');
+    row.offer_price_string = numeral(row.offer_price).format('0,0');
+    
     res.render('viewProduct/product', {
         product: row
     })
