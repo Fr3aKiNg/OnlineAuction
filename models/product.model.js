6c9edf51090aafeaed21bef8315fc5d677d50dbd
chihuynhminh <chihuynhminh@gmail.com>
2020-01-08 16:25:04 +0700

    Get top 5

--- models/product.model.js
+++ models/product.model.js
@@ -10,7 +10,7 @@ module.exports = {
         return rows[0].total;
     },
     pageByCat: (catId, offset) => db.load(`select * from products where category_id = ${catId} limit ${config.pagination.limit} offset ${offset}`),
-    getHighestPrice: _ => db.load('SELECT * FROM products ORDER BY offer_price LIMIT 5'),
-    getAlmostEndTime: _ => db.load('SELECT * FROM products ORDER BY end_time LIMIT 5;'),
-    getTopBiddingTurn: _ => db.load('SELECT * FROM products ORDER BY end_time LIMIT 5;'),
+    getHighestPrice: _ => db.load('SELECT * FROM products WHERE end_time > CURRENT_TIMESTAMP() ORDER BY offer_price LIMIT 5'),
+    getAlmostEndTime: _ => db.load('SELECT * FROM products WHERE end_time > CURRENT_TIMESTAMP() ORDER BY end_time LIMIT 5;'),
+    getTopBiddingTurn: _ => db.load('SELECT * FROM products p WHERE end_time > CURRENT_TIMESTAMP() ORDER BY (select count(*) from offers o where p.product_id = o.product_id) DESC  LIMIT 5;'),
 };
