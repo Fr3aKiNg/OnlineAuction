3bcc5f65a41ee5837a119750d8838e0a3d6f00dc
VO Quoc An <hvcgtraining@gmail.com>
2020-01-08 21:42:43 +0700

    update profile

--- routes/product.route.js
+++ routes/product.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/:id', async function(req, res) {
     const rows = await productModel.single(req.params.id);
+    console.log(JSON.stringify(rows[0]));
     res.render('viewProduct/product', {
         product: rows[0]
     })
