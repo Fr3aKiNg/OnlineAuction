6e4b9a95779f0b482df5bf7ceacc6f1fb1a226d6
chihuynhminh <chihuynhminh@gmail.com>
2020-01-08 22:12:57 +0700

    single function product

--- models/product.model.js
+++ models/product.model.js
@@ -4,20 +4,6 @@ const config = require('../config/default.json');
 module.exports = {
     all: _ => db.load('select * from products'),
 
-    single = function(id) {
-        p = db.load(`select p.product_id, p.name, 
-        us.username as seller_name,
-        uw.username as winner_name,
-        p.posted_time, p.end_time
-        from products p, users us, users uw
-        where p.product_id = ${id} and p.winner_id = uw.user_id and p.seller_id = us.user_id;
-        `);
-
-        p.rate_seller = db.load(`select (sum(e.value) * 100 / count(*)) as rate_seller from evaluates e WHERE e.tar_user_id = ${p.seller_id};`);
-        p.rate_winner = db.load(`select (sum(e.value) * 100 / count(*)) as rate_seller from evaluates e WHERE e.tar_user_id = ${p.winner_id};`);
-        
-        return p;
-    },
     offers: id => db.load(`select o._time, u.username, o.money from offers o, users u where o.user_id = o.user_id and o.product_id = ${id}`),
 
     allByCat: catId => db.load(`select * from products where category_id = ${catId}`),
@@ -50,4 +36,19 @@ module.exports = {
     WHERE o.product_id = p.product_id AND end_time > CURRENT_TIMESTAMP()\
     GROUP BY p.product_id, p.name, u.username, p.offer_price, p.end_time\
     ORDER BY (select count(*) from offers o where p.product_id = o.product_id) DESC  LIMIT 5;'),
+};
+
+module.exports.single = function(id) {
+    p = db.load(`select p.product_id, p.name, 
+    us.username as seller_name,
+    uw.username as winner_name,
+    p.posted_time, p.end_time
+    from products p, users us, users uw
+    where p.product_id = ${id} and p.winner_id = uw.user_id and p.seller_id = us.user_id;
+    `);
+
+    p.rate_seller = db.load(`select (sum(e.value) * 100 / count(*)) as rate_seller from evaluates e WHERE e.tar_user_id = ${p.seller_id};`);
+    p.rate_winner = db.load(`select (sum(e.value) * 100 / count(*)) as rate_seller from evaluates e WHERE e.tar_user_id = ${p.winner_id};`);
+    
+    return p;
 };
