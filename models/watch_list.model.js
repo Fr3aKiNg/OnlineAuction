fdcecd9db928bd30387a08dd2249f2eea90313d2
chihuynhminh <chihuynhminh@gmail.com>
2020-01-10 11:56:25 +0700

    done watch list

--- models/watch_list.model.js
+++ models/watch_list.model.js
@@ -4,7 +4,11 @@ module.exports = {
   add: entity => db.add(entity, 'watch_list'),
 
   loadByUserId: async user_id => {
-    const rows = await db.load(`select * from watch_list where user_id = '${user_id}'`);
+    const rows = await db.load(`SELECT p.product_id as product_id, p.name as name, \
+    u.username AS winner_username, p.offer_price AS offer_price, p.end_time AS end_time, p.posted_time AS posted_time, COUNT(*) AS count_offers \
+    FROM (products p JOIN users u ON u.user_id = p.winner_id) LEFT JOIN offers o ON o.product_id = p.product_id \
+    WHERE end_time > CURRENT_TIMESTAMP() AND p.product_id IN (select product_id from watch_list where user_id = ${user_id})\
+    GROUP BY p.product_id, p.name, u.username, p.offer_price, p.end_time, p.posted_time`);
     return rows;
   }
 };
--- routes/watch_list.route.js
+++ routes/watch_list.route.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const moment = require('moment')
+const numeral = require('numeral')
+
 const config = require('../config/default.json');
 const watchListModel = require('../models/watch_list.model')
 
 const router = express.Router();
 
-
 const restrict = require('../middlewares/auth.mdw');
 router.get('/', restrict, async function(req, res) {
-    console.log(req.session.authUser);
-    watchPro = await watchListModel.loadByUserId(req.session.authUser.user_id);
-    console.log(watchPro);
-    res.render('viewProduct/watchlist', {});
+    products = await watchListModel.loadByUserId(req.session.authUser.user_id);
+    
+    for (var i in products) {
+        products[i].offer_price = numeral(products[i].offer_price).format('0,0');
+        products[i].isNew = (Date.now() - products[i].posted_time.valueOf()) / 1000 / 60 < 60;
+        products[i].isEndSoon = (products[i].end_time - Date.now()) / 1000 / 60 / 60 / 24 < 7;
+        if (products[i].isEndSoon)
+            products[i].end_time = moment(products[i].end_time).fromNow();
+        else
+            products[i].end_time = moment(products[i].end_time).format('LLL');
+    }
+
+    res.render('viewProduct/watchlist', {
+        searchItem: products
+    });
 })
 
 module.exports = router;
